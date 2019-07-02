263a4f1b4845263bb5d814bd47c8748b1919b8ed
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-02 16:58:15 -0700

    Updates With useEffects

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,15 +4,15 @@ import axios from 'axios'
 const ResourceList = ({ resource }) => {
     const [resources, setResources ] = useState([]);
 
-    const fetchResource = async (response) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${response}`);
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
         setResources(response.data);
     }
 
     useEffect(()=>{
         fetchResource(resource);
-    }, []);
+    }, [resource]);
 
     return (
         <div>{resources.length}</div>
