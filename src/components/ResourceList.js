a09a9b3c39e0be5674288608a7efcb2eb0de1f91
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-02 17:40:54 -0700

    Rendering a List

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -13,7 +13,7 @@ const ResourceList = ({ resource }) => {
     }, [resource]);
 
     return (
-        <div>{resources.length}</div>
+        <ul>{resources.map(record =><li key={record.id}>{record.title}</li>)}</ul>
     );
 }
 
